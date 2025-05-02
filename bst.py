67536a5349d719ed8ca98f61455a61248b92279a
zenithlalhere <145013928+zenithlalhere@users.noreply.github.com>
2025-05-03 04:58:52 +0530

    Create bst.py

---
+++ bst.py
@@ -0,0 +1,104 @@
+class BSTNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return BSTNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            temp = self._min_value_node(node.right)
+            node.key = temp.key
+            node.right = self._delete(node.right, temp.key)
+        return node
+
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def inorder(self):
+        self._inorder(self.root)
+        print()
+
+    def _inorder(self, node):
+        if node:
+            self._inorder(node.left)
+            print(node.key, end=' ')
+            self._inorder(node.right)
+
+    def preorder(self):
+        self._preorder(self.root)
+        print()
+
+    def _preorder(self, node):
+        if node:
+            print(node.key, end=' ')
+            self._preorder(node.left)
+            self._preorder(node.right)
+
+    def postorder(self):
+        self._postorder(self.root)
+        print()
+
+    def _postorder(self, node):
+        if node:
+            self._postorder(node.left)
+            self._postorder(node.right)
+            print(node.key, end=' ')
+
+# Example usage
+bst = BST()
+for val in [50, 30, 70, 20, 40, 60, 80]:
+    bst.insert(val)
+
+print("Inorder:")
+bst.inorder()
+print("Preorder:")
+bst.preorder()
+print("Postorder:")
+bst.postorder()
+
+print("Searching for 60:", bst.search(60) is not None)
+bst.delete(70)
+print("After deleting 70, Inorder:")
+bst.inorder()
